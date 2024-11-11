48c20d08ab29bfe3f549b5641274d90ba6289448
NguyenHuuHung <hhnguyenit@gmail.com>
2024-11-11 20:12:34 +0700

    config deploy

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost: "Udagram-api-dev.eba-ddqttwif.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
